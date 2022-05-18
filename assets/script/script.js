ae3dacfaf703d65c0265805d4d78374eb9f1cc7b
Renaldas Bendikas <renaldasb1999@gmail.com>
2022-05-18 12:45:17 +0000

    add first 6 questions into question variable

---
+++ assets/script/script.js
@@ -0,0 +1,65 @@
+let questions = [
+    {
+        question:'1. Which team has won the most constructors championships?',
+        answers: {
+            A: 'Ferrari',
+            B: 'McLaren',
+            C: 'Williams',
+            D: 'Mercedes'
+        },
+        correctAnswer: 'Ferrari'
+    },
+    {
+        question:'2. Which driver has won the most consequtive drivers championships?',
+        answers: {
+            A: 'Lewis Hamilton',
+            B: 'Sebastian Vettel',
+            C: 'Ayrton Senna',
+            D: 'Michael Schumacher'
+        },
+        correctAnswer: 'Michael Schumacher'
+    },
+    {
+        question:'3. Which driver has won the most races in one season?',
+        answers: {
+            A: 'Michael Schumacher',
+            B: 'Jenson Button',
+            C: 'Kimi Raikkonen',
+            D: 'Ayrton Senna'
+        },
+        correctAnswer: 'Michael Schumacher'
+    },
+    {
+        question:'4. What was the highest number of points scored by a driver in one season?',
+        answers: {
+            A: '397',
+            B: '405',
+            C: '296',
+            D: '360'
+        },
+        correctAnswer: '397'
+    },
+    {
+        question:'5. Which team has been part of F1 the longest?',
+        answers: {
+            A: 'Williams',
+            B: 'McLaren',
+            C: 'Ferrari',
+            D: 'RedBull'
+        },
+        correctAnswer: 'Ferrari' 
+    },
+    {
+        question:'6. Which team has scored the most points in a season (current scoring)?',
+        answers: {
+            A: 'RedBull',
+            B: 'Ferrari',
+            C: 'Mercedes',
+            D: 'Alpha Tauri'
+        },
+        correctAnswer: 'Mercedes'
+    },
+    {
+
+    }
+]
